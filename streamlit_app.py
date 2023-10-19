0a0fab4cb148d785179f8e29bf0b2dbb10151d83
prashantsomani <35595404+prashantsomani@users.noreply.github.com>
2023-10-19 13:02:27 -0400

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-import streamlist 
-streamlist.title("My Parents New Healthy Dinner")
+import streamlit 
+streamlit.title("My Parents New Healthy Dinner")
 
-streamlist.header("Breakfast Menu")
-streamlist.text("Omega 3 & Blueberry Oatmeal")
-streamlist.text("Kale, Spinach & Rocket Smoothie")
-streamlist.text("Hard-Boiled free-range Egg")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled free-range Egg")
